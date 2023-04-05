74e6fea40270e8a1e0cac2df8b4929c879c7faaa
38571 <dennis.mustafic@stud.hs-kempten.de>
2023-04-05 09:59:34 +0200

    Created struct, that holds task specific informations

---
+++ helpers/task.py
@@ -0,0 +1,9 @@
+TASK_DATA = {
+    "ecthr_a": [10, "multi_label"],
+    "ecthr_b": [10, "multi_label"],
+    "scotus": [14, "multi_class"],
+    "eurlex": [100, "multi_label"],
+    "ledgar": [100, "multi_class"],
+    "unfair_tos": [8, "multi_label"],
+    "case_hold": [5, "multiple_choice"]
+}
