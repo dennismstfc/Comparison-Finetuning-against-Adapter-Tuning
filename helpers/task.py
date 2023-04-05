4cd26680c529340179aa9f5a0fa9ea17ce1b84d2
38571 <dennis.mustafic@stud.hs-kempten.de>
2023-04-05 10:19:46 +0200

    Delted the old folder, which is now called 'src'

--- helpers/task.py
+++
@@ -1,9 +0,0 @@
-TASK_DATA = {
-    "ecthr_a": [10, "multi_label"],
-    "ecthr_b": [10, "multi_label"],
-    "scotus": [14, "multi_class"],
-    "eurlex": [100, "multi_label"],
-    "ledgar": [100, "multi_class"],
-    "unfair_tos": [8, "multi_label"],
-    "case_hold": [5, "multiple_choice"]
-}
