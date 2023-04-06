624e64f48543a69c6d042cade7cc72b46426caaf
38571 <dennis.mustafic@stud.hs-kempten.de>
2023-04-06 11:53:33 +0200

    Added function that returns the paths of task-specific output folder and log subdirectory. This is needed for the training_args. Also fixed bugs in the create_output_folder()-function

--- src/utils.py
+++ src/utils.py
@@ -6,8 +6,8 @@ outputs of the model during training.
 '''
 def create_output_folder(task_name, base_dir = "output"):
     # First check if the general output folder exists
-    if not os.path.exists("output"):
-        os.mkdir("output")
+    if not os.path.exists(base_dir):
+        os.mkdir(base_dir)
     
     # Create the task specific 
     task_dir = os.path.join(base_dir, task_name)
@@ -18,3 +18,8 @@ def create_output_folder(task_name, base_dir = "output"):
     log_dir = os.path.join(task_dir, "log")
     if not os.path.exists(log_dir):
         os.mkdir(log_dir)
+
+# Returns the paths to a task-specific output folder and log subdirectory
+def get_output_logging_paths(task_name, base_dir="output"):
+    task_dir = os.path.join(base_dir, task_name)
+    return task_dir, os.path.join(task_dir, "log") 
