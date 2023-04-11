2005086c0bde662b46ad2ae04f6fb2c1d91be3ce
38571 <dennis.mustafic@stud.hs-kempten.de>
2023-04-11 14:27:52 +0200

    Removed unneccessary adapter.py file. The adapter module gets now in the train_adapter_model.py file

--- src/adapter.py
+++
@@ -1,8 +0,0 @@
-from transformers.adapters import AdapterConfig
-
-adapter_config = AdapterConfig(
-    mh_adapter=True, 
-    output_adapter=True, 
-    reduction_factor=16, 
-    non_linearity="relu"
-    )
