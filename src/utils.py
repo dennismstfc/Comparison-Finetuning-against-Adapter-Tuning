e012f343e6462e6f99efea25db42513e48802ea9
38571 <dennis.mustafic@stud.hs-kempten.de>
2023-04-11 14:45:06 +0200

    Changed output path parameter

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import os
 This function creates based on task_name a folder, that is used to store the
 outputs of the model during training.
 '''
-def create_output_folder(task_name, base_dir = "output"):
+def create_output_folder(task_name, base_dir = "../output"):
     # First check if the general output folder exists
     if not os.path.exists(base_dir):
         os.mkdir(base_dir)
@@ -20,6 +20,6 @@ def create_output_folder(task_name, base_dir = "output"):
         os.mkdir(log_dir)
 
 # Returns the paths to a task-specific output folder and log subdirectory
-def get_output_logging_paths(task_name, base_dir="output"):
+def get_output_logging_paths(task_name, base_dir="../output"):
     task_dir = os.path.join(base_dir, task_name)
     return task_dir, os.path.join(task_dir, "log") 
