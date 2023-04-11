5fb81c4a268cda7ad016d966dabd27cef5436ab9
38571 <dennis.mustafic@stud.hs-kempten.de>
2023-04-11 14:22:55 +0200

    Created run_experiment.py-file which trains the base model and afterwars the adapter model

---
+++ src/run_experiment.py
@@ -0,0 +1,33 @@
+from train_adapter_models import start_adapter_tuning
+from train_base_models import start_vanilla_finetuning
+from task import TASK_DATA
+
+def run_experiment_all(checkpoint):
+    for actual_task in TASK_DATA.keys():
+        start_vanilla_finetuning(
+            checkpoint=checkpoint,
+            actual_task=actual_task
+        )
+
+        start_adapter_tuning(
+            checkpoint=checkpoint,
+            actual_task=actual_task,
+            adapter_name="bottlneck_adapter"
+        )
+
+
+
+if __name__ == "__main__":
+    checkpoint = "bert-base-cased"    
+    actual_task = "unfair_tos"
+    
+    start_vanilla_finetuning(
+        checkpoint=checkpoint,
+        actual_task=actual_task
+    )
+
+    start_adapter_tuning(
+        checkpoint=checkpoint,
+        actual_task=actual_task,
+        adapter_name="bottlneck_adapter"
+    )
