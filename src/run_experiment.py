5de0f9ea3ad8ade4ab6441e86b6f65c42dae6671
38571 <dennis.mustafic@stud.hs-kempten.de>
2023-04-13 12:21:36 +0200

    Changed experiment setup

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -21,15 +21,23 @@ if __name__ == "__main__":
     checkpoint = "bert-base-cased"    
     actual_task = "unfair_tos"
     
+    # 24 hours in seconds
+    #train_duration = 24 * 60 * 60
+    train_duration = 2 * 60 * 60
+
     start_vanilla_finetuning(
         checkpoint=checkpoint,
         actual_task=actual_task,
-        seed=110110
+        seed=110110,
+        train_duration=train_duration,
+        early_stopping_patience=3
     )
 
     start_adapter_tuning(
         checkpoint=checkpoint,
         actual_task=actual_task,
         adapter_name="bottlneck_adapter",
-        seed=110110
+        seed=110110,
+        train_duration=train_duration,
+        early_stopping_patience=3
     )
