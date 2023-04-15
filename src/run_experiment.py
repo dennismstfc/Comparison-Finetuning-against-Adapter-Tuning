d7f4276465f8972b82f92f0fd6cf3df7593b97ca
38571 <dennis.mustafic@stud.hs-kempten.de>
2023-04-15 16:11:24 +0200

    Changed expermiment design and added error logging if the training process fails

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -1,60 +1,60 @@
 from train_adapter_models import start_adapter_tuning
 from train_base_models import start_vanilla_finetuning
 from task import TASK_DATA
-
-def run_experiment_all(
-        checkpoint: str,
-        train_duration: float,
-        early_stopping_patience: int
-        ) -> None:
-    for actual_task in TASK_DATA.keys():
-        start_vanilla_finetuning(
-            checkpoint=checkpoint,
-            actual_task=actual_task
-            seed=110110,
-            train_duration=train_duration,
-            early_stopping_patience=early_stopping_patience
-        )
-
-        start_adapter_tuning(
-            checkpoint=checkpoint,
-            actual_task=actual_task,
-            adapter_name="bottlneck_adapter"
-            seed=110110,
-            train_duration=train_duration,
-            early_stopping_patience=early_stopping_patience
-        )
+from utils import write_error_log
 
 
+'''
+This function trains all downstream models with vanilla finetuning for 
+a given amount of time. 
+'''
 def run_vanilla_finetuning_all(
         checkpoint: str,
         train_duration: float,
         early_stopping_patience: int
         ) -> None:
+
+    print("Starting Vanilla Finetuning for all tasks.")
+    print(f"Each downstream model gets trained for {train_duration / {60 * 60}} h.")
     for actual_task in TASK_DATA.keys():
-        start_vanilla_finetuning(
-            checkpoint=checkpoint,
-            actual_task=actual_task,
-            seed=110110,
-            train_duration=train_duration,
-            early_stopping_patience=early_stopping_patience
-        )
+        try:
+            start_vanilla_finetuning(
+                checkpoint=checkpoint,
+                actual_task=actual_task,
+                seed=110110,
+                train_duration=train_duration,
+                early_stopping_patience=early_stopping_patience
+            )
+        except Exception as e:
+            print(f"Training for {actual_task} failed. Contuning with the next task.")
+            write_error_log(actual_task, str(e))
 
 
+'''
+This function trains all downstream models with adapter tuning for a given
+amount of time. 
+'''
 def run_adapter_tuning_all(
         checkpoint: str,
         train_duration: float,
         early_stopping_patience: int
         ) -> None:
+
+    print("Starting Adapter Tuning for all tasks.")
+    print(f"Each downstream model gets trained for {train_duration / {60 * 60}} h.")
     for actual_task in TASK_DATA.keys():
-        start_adapter_tuning(
-            checkpoint=checkpoint,
-            actual_task=actual_task,
-            adapter_name="bottleneck_adapter",
-            seed=110110,
-            train_duration=train_duration,
-            early_stopping_patience=early_stopping_patience
-        )
+        try:
+            start_adapter_tuning(
+                checkpoint=checkpoint,
+                actual_task=actual_task,
+                adapter_name="bottleneck_adapter",
+                seed=110110,
+                train_duration=train_duration,
+                early_stopping_patience=early_stopping_patience
+            )
+        except Exception as e:
+            print(f"Training for {actual_task} failed. Contuning with the next task.")
+            write_error_log(actual_task, str(e))
 
 
 if __name__ == "__main__":
